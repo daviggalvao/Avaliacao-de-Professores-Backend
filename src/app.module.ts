542ac34d30be395c3943b4f49ea2d2c79592388d
gusfring41 <gusfring.a@gmail.com>
2024-11-21 15:45:59 -0300

    feat(comentario,avaliacao): module files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { AvaliacaoModule } from './avaliacao/avaliacao.module';
+import { ComentarioModule } from './comentario/comentario.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AvaliacaoModule, ComentarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
