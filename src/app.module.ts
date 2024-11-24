3c38e713540053e7fc1ef542b1c7d63199f63d20
Leo3107 <leonardokrauss3107@gmail.com>
2024-11-23 23:20:45 -0300

    New branch
    Branch to start developing new data base elements

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { UserModule } from './user/user.module';
 import { AvaliacaoModule } from './avaliacao/avaliacao.module';
 import { ComentarioModule } from './comentario/comentario.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProfessorModule } from './professor/professor.module';
+import { DisciplinaService } from './disciplina/disciplina.service';
+import { DisciplinaController } from './disciplina/disciplina.controller';
+import { DisciplinaModule } from './disciplina/disciplina.module';
 
 @Module({
-  imports: [UserModule, AvaliacaoModule, ComentarioModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, AvaliacaoModule, ComentarioModule, PrismaModule, ProfessorModule, DisciplinaModule],
+  controllers: [AppController, DisciplinaController],
+  providers: [AppService, DisciplinaService],
 })
 export class AppModule {}
