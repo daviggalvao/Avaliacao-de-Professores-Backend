1f5c54f95526de692f7cbb719f3151c2d1648f13
Leo3107 <leonardokrauss3107@gmail.com>
2025-05-28 14:47:49 -0300

    QF

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Ou use '*' para permitir qualquer origem
+    origin: 'http://172.29.38.217:3000', // Ou use '*' para permitir qualquer origem
     methods: 'GET,POST,PUT,DELETE,PATCH', // Métodos permitidos
     allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
   });
